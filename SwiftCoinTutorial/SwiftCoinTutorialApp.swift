6b27017b33daa633525bbf147307510409db0e99
sasaki <sasaking0215@outlook.jp>
2023-08-01 15:34:12 +0900

    Initial Commit

---
+++ SwiftCoinTutorial/SwiftCoinTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCoinTutorialApp.swift
+//  SwiftCoinTutorial
+//
+//  Created by Mankuki_era on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCoinTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
