01595e5d578cbfbd4904ce4f3d9e26a33ca3ac4c
sasaki <sasaking0215@outlook.jp>
2023-08-02 14:49:49 +0900

    first commit

---
+++ SwiftCoinTutorial/Core/Home/Views/AllCoinsView.swift
@@ -0,0 +1,44 @@
+//
+//  AllCoinsView.swift
+//  SwiftCoinTutorial
+//
+//  Created by Mankuki_era on 2023/08/01.
+//
+
+import SwiftUI
+
+struct AllCoinsView: View {
+    @StateObject var viewModel: HomeViewModel
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("All Coins")
+                .font(.headline)
+                .padding()
+            
+            HStack {
+                Text("Coin")
+                
+                Spacer()
+                
+                Text("Price")
+            }
+            .foregroundColor(.gray)
+            .font(.caption)
+            .padding(.horizontal)
+            
+            ScrollView {
+                VStack {
+                    ForEach(viewModel.coins) { coin in
+                        CoinRowView(coin: coin)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AllCoinsView_Previews: PreviewProvider {
+    static var previews: some View {
+        AllCoinsView(viewModel: HomeViewModel())
+    }
+}
---
+++ SwiftCoinTutorial/Core/Home/Views/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  SwiftCoinTutorial
+//
+//  Created by Mankuki_era on 2023/08/01.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @StateObject var viewModel = HomeViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView(.vertical, showsIndicators: false) {
+                // top movers view
+                TopMoversView(viewModel: viewModel)
+                
+                Divider()
+                
+                // all coins view
+                AllCoinsView(viewModel: viewModel)
+            }
+            .navigationTitle("Live Prices")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
